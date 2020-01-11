602767f66146991b97af47c4d2e06ad8243328db
simisoluwa <Adeyemo1882@gmail.com>
2020-01-11 11:11:01 +0100

    Added dependecies for the server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.listen(() => {});
+app.get("/", (req, res) => {
+  res.send({ message: "hello there 😷" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
