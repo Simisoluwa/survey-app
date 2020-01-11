3aa7f442012571ffb3166a9143e23fce6ea220c7
simisoluwa <Adeyemo1882@gmail.com>
2020-01-11 11:30:41 +0100

    Modified the server.js file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "hello there 😷" });
+  res.send({ message: "Alright bye bye" });
 });
 
 const PORT = process.env.PORT || 5000;
